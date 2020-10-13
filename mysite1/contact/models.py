719d82d6d3c0b1456eaca0ea51567017f61e1a38
harshvardhan219 <44029089+harshvardhan219@users.noreply.github.com>
2020-10-13 22:21:15 +0530

    1.6

---
+++ mysite1/contact/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+STATUS_CHOICES = (
+    ('Employed', 'Employed'),
+    ('Unemployed', 'Unemployed'),
+    ('self-employed', 'self-employed'),
+)
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.CharField(max_length=12)
+    email = models.EmailField()
+    mobile_number = models.CharField(max_length=12)
+    pincode = models.CharField(max_length=10)
+    highest_qualification = models.CharField(max_length=100)
+    university = models.CharField(max_length=100)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
