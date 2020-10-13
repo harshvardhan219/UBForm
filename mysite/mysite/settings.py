2223b2fc8694e1e58372c768e9b0475aa8f79ddd
harshvardhan219 <44029089+harshvardhan219@users.noreply.github.com>
2020-10-13 21:57:37 +0530

    1.5

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@vh-sfqfmoprjpvwa_159!dqg&4x0%_unbn=8(@bqja+6affi$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000','formccl.herokuapp.com']
 
 
 # Application definition
