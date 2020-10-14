9384f65a73ada56fc71124b995f131c648d64d57
Sachin Roy <sachinroy784@gmail.com>
2020-10-14 16:15:15 +0530

    Fixed contact models, settings config

--- contact/models.py
+++ contact/models.py
@@ -2,14 +2,18 @@ from django.db import models
 
 # Create your models here.
 
-STATUS_CHOICES = (
-    ('Employed', 'Employed'),
-    ('Unemployed', 'Unemployed'),
-    ('self-employed', 'self-employed'),
-)
-
+class STATUS(object):
+    EMPLOYED = 1
+    UNEMPLOYED = 2
+    SELF_EMPLOYED = 3
 
 class Contact(models.Model):
+    STATUS_CHOICES = (
+        (STATUS.EMPLOYED, 'Employed'),
+        (STATUS.UNEMPLOYED, 'Unemployed'),
+        (STATUS.SELF_EMPLOYED, 'self-employed'),
+    )
+
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     age = models.CharField(max_length=12)
@@ -18,6 +22,6 @@ class Contact(models.Model):
     pincode = models.CharField(max_length=10)
     highest_qualification = models.CharField(max_length=100)
     university = models.CharField(max_length=100)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='')
+    status = models.PositiveSmallIntegerField(choices=STATUS_CHOICES, null=True, blank=True)
 
 
